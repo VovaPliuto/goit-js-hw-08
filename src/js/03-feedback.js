b0820f0620aed10584c15eccac3dd8f0a23d8558
VovaPliuto <vovaplyuto@gmail.com>
2023-05-01 10:03:59 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,17 +14,17 @@ if (localStorage.getItem('feedback-form-state') !== null) {
 
   if (dataObj.email !== undefined) {
     refs.formEl.firstElementChild.children[0].value = dataObj.email.trim();
-  } else { 
-    refs.formEl.firstElementChild.children[0].value = "";
+  } else {
+    refs.formEl.firstElementChild.children[0].value = '';
   }
 
   if (dataObj.message !== undefined) {
     refs.formEl.firstElementChild.nextElementSibling.children[0].value =
       dataObj.message.trim();
-  } else { 
+  } else {
     refs.formEl.firstElementChild.nextElementSibling.children[0].value = '';
   }
-};
+}
 
 function onFormInput(e) {
   if (e.target.name === 'email') {
@@ -35,21 +35,25 @@ function onFormInput(e) {
     dataObj.message = e.target.value;
     localStorage.setItem('feedback-form-state', JSON.stringify(dataObj));
     console.log(localStorage);
-  
   }
 }
 
-function onSubmitForm(e) { 
+function onSubmitForm(e) {
   e.preventDefault();
 
-  if (dataObj.email === undefined || dataObj.message === undefined) { 
-    return alert('Поля мають бути заповнені')
-  };
+  if (
+    dataObj.email === undefined ||
+    dataObj.email === '' ||
+    dataObj.message === undefined ||
+    dataObj.message === ''
+  ) {
+    return alert('Всі поля мають бути заповнені');
+  }
 
   console.log(dataObj);
   localStorage.clear();
   dataObj = {};
 
-  refs.formEl.firstElementChild.children[0].value = "";
-  refs.formEl.firstElementChild.nextElementSibling.children[0].value = "";
+  refs.formEl.firstElementChild.children[0].value = '';
+  refs.formEl.firstElementChild.nextElementSibling.children[0].value = '';
 }
